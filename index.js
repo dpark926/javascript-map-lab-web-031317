bd63f1703b4594649e94cdf6ff2ead8c75637896
Daniel Park <dpeace926@gmail.com>
2017-04-26 00:33:28 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const robots = [
   { name: 'Ratchet', alliance: null }
 ];
 
+var sortedRobots = robots.map(function(robot) {
+  if (knownDecepticons.includes(robot.name)) {
+    return Object.assign({}, robot, {
+      alliance: "decepticon"
+    })
+  } else {
+    return Object.assign({}, robot, {
+      alliance: "autobot"
+    })
+  }
+})
+
+// console.log(sortedRobots)
+
 const zebraStripes = [
   { width: 9.12, color: null },
   { width: 5.71, color: null },
@@ -21,3 +35,18 @@ const zebraStripes = [
   { width: 0.59, color: null },
   { width: 7.31, color: null }
 ];
+
+var coloredZebraStripes = zebraStripes.map(function(eachZebra) {
+  if (zebraStripes.indexOf(eachZebra) % 2 === 0) {
+    return Object.assign({}, eachZebra, {
+      color: "white"
+    })
+  } else {
+    return Object.assign({}, eachZebra, {
+      color: "black"
+    })
+  }
+  // return zebraStripes.indexOf(eachZebra) % 2 === 0
+})
+
+// console.log(coloredZebraStripes)
